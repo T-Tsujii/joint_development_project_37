861575c4c6dd01ebda300b8c4ab42bba54beb53f
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-11-07 09:02:27 +0900

    詳細表示機能実装完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,18 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+
   def new
     @message = Message.new
   end
 
   def create
     Message.create(message_params)
+    # message = Message.create!(message_params)
+    # redirect_to message
   end
 
   private
