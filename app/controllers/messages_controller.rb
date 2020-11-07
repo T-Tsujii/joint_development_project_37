acb64c0d56ca074d721d2fae1cf83ab709037f3e
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-11-07 09:20:49 +0900

    編集機能実装完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,15 @@ class MessagesController < ApplicationController
     # redirect_to message
   end
 
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    message.update(message_params)
+  end
+
   private
 
   def message_params
